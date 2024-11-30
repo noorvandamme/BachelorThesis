f869a7ebe837826e0918a9e223bceec047be26df
noorvandamme <noor.vandamme@student.howest.be>
2024-11-30 10:54:57 +0100

    no https

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
-const isDevelopment = (process.env.NODE_ENV === 'development');
+// const isDevelopment = (process.env.NODE_ENV === 'development');
 const express = require('express');
 const app = express();
-const fs = require('fs');
+// const fs = require('fs');
 
-let options = {};
-if (isDevelopment) {
-  options = {
-    key: fs.readFileSync('./localhost.key'),
-    cert: fs.readFileSync('./localhost.crt')
-  };
-}
+const port = process.env.PORT || 3000;
 
-const server = require(isDevelopment ? 'https' : 'http').Server(options, app);
-const port = process.env.PORT || 443;
+//HTTP-server
+const server = require('http').Server(app);
 
 app.use(express.static('public'));
 
+//start server 
 server.listen(port, () => {
- console.log(`App listening on port ${port}!`);
-});
+  console.log(`App listening on port ${port}!`);
+ });
+
+
+
+
+
